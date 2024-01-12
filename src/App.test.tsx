01cb34d98ac782b9cf0fab5673c56a9aa4bc625e
Nicodemus Nutsukpui <nicodemus.nutsukpui@platform24.se>
2024-01-12 15:47:48 +0100

    Fix ans setup testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders App component without crashing", () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+  });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import "./setupDomTests";
+
+Object.defineProperty(window, "ResizeObserver", {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  })),
+});
+
+jest.mock("@formkit/auto-animate/react", () => ({
+  useAutoAnimate: () => [null],
+}));
